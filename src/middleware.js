591e5604c54ceb06fb472e0084ee1a0e4cf1abcf
kirbylane505 <kirbylane505@users.noreply.github.com>
2026-02-04 00:37:59 -0500

    Force redeploy - site should be public

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 // Password protection DISABLED - site is now public
+// Force redeploy: 2026-02-04
 export function middleware(request) {
   // Allow all access - no password required
   return NextResponse.next();
